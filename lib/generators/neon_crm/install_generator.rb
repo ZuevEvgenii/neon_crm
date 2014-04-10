68f102c5fc46747b8b5126f099509d607b89be6a
Zuev Evgenii <zuev.evgenii@gmail.com>
2014-04-10 20:34:47 +0600

    Fix templates path

--- lib/generators/neon_crm/install_generator.rb
+++ lib/generators/neon_crm/install_generator.rb
@@ -1,7 +1,7 @@
 module NeonCrm
   module Generators
     class InstallGenerator < Rails::Generators::Base
-      source_root File.expand_path("../templates", __FILE__)
+      source_root File.expand_path("../../templates", __FILE__)
 
       def copy_networks
         template "networks.yml", "config/networks.yml"
